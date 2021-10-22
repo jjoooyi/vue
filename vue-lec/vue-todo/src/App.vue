8bb24585908d0b3dbc9f5770e72e3a60a46cdebe
LAPTOP-LHPVDHS1\TA9 <jjoooyi@gmail.com>
2021-10-22 14:13:23 +0900

    vuex refactoring ~ vue-todo --211022

--- vue-lec/vue-todo/src/App.vue
+++ vue-lec/vue-todo/src/App.vue
@@ -43,15 +43,15 @@ export default {
       this.todoItems = [];
     }
   },
-  created() {
-    if (localStorage.length > 0) {
-      for (let i=0; i<localStorage.length; i++) {
-        if (localStorage.key(i) !== 'loglevel:webpack-dev-server') {
-          this.todoItems.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
-        }
-      }
-    }
-  },
+  // created() {
+  //   if (localStorage.length > 0) {
+  //     for (let i=0; i<localStorage.length; i++) {
+  //       if (localStorage.key(i) !== 'loglevel:webpack-dev-server') {
+  //         this.todoItems.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
+  //       }
+  //     }
+  //   }
+  // },
   components: {
     //컴포넌트 태그명: 컴포넌트 내용, 향상된 객체 리터럴
     // 'TodoHeader': TodoHeader,
